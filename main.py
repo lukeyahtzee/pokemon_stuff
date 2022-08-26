407051e99589fa19c224f135136132580a659e07
Josh Prock <jprock@evolytics.com>
2022-08-26 11:31:36 -0500

    added name == main to main.py

--- main.py
+++ main.py
@@ -4,15 +4,19 @@ from battle import Battle
 from pokedex import check_pokedex, pokedex
 
 
-choice1 = check_pokedex(pokedex, 'Please pick a Pokemon: ')
-choice2 = check_pokedex(pokedex, 'Please select a second Pokemon: ')
+def main():
+    choice1 = check_pokedex(pokedex, 'Please pick a Pokemon: ')
+    choice2 = check_pokedex(pokedex, 'Please select a second Pokemon: ')
+
+    pokemon1 = Pokemon(choice1)
+    pokemon2 = Pokemon(choice2)
+
+    print('\n')
+
+    battle = Battle(pokemon1, pokemon2)
+
+    battle.commence_battle()
 
 
-pokemon1 = Pokemon(choice1)
-pokemon2 = Pokemon(choice2)
-
-print('\n')
-
-battle = Battle(pokemon1, pokemon2)
-
-battle.commence_battle()
+if __name__ == '__main__':
+    main()
